16c03ac241dac01cc05500b29df21234630a4fd1
Joao Costa <miguel-cds@hotmail.com>
2024-07-08 23:30:00 +0200

    5.1

---
+++ ejercicio5.js
@@ -0,0 +1,28 @@
+// Ejercicio 5
+
+const albums = [
+    { title: 'Led Zeppelin IV', genre: 'Rock', duration: 42.19 },
+    { title: 'The Dark Side of the Moon', genre: 'Rock', duration: 42.49 },
+    { title: 'Back in Black', genre: 'Rock', duration: 42.11 },
+    { title: 'Hotel California', genre: 'Rock', duration: 43.08 },
+    { title: 'Abbey Road', genre: 'Rock', duration: 47.23 },
+    { title: 'Thriller', genre: 'Pop', duration: 42.19 },
+    { title: 'A Night at the Opera', genre: 'Rock', duration: 43.08 },
+    { title: 'The Wall', genre: 'Rock', duration: 81.00 },
+    { title: 'Born to Run', genre: 'Rock', duration: 39.26 },
+    { title: 'The Joshua Tree', genre: 'Rock', duration: 50.11 }
+  ];
+  
+  // Añade tu código de bucle aquí
+  
+  let total = 0;
+  
+  for (const item of albums) {
+    if (item.genre === "Rock") {
+      
+      total += item.duration;
+    }
+   
+  }
+  
+  console.log( "La duracion total de solo Rock es de: ", total);
