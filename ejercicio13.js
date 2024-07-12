973a4d09b5a10309a7e8ce34eabf32a5662f4fd4
Joao Costa <miguel-cds@hotmail.com>
2024-07-13 01:51:28 +0200

    13

---
+++ ejercicio13.js
@@ -0,0 +1,33 @@
+const actors = [
+    { name: 'Leonardo DiCaprio', born: 1974 },
+    { name: 'Tom Hanks', born: 1956 },
+    { name: 'Meryl Streep', born: 1949 },
+    { name: 'Brad Pitt', born: 1963 },
+    { name: 'Johnny Depp', born: 1963 },
+    { name: 'Scarlett Johansson', born: 1984 },
+    { name: 'Jennifer Lawrence', born: 1990 },
+    { name: 'Denzel Washington', born: 1954 },
+    { name: 'Morgan Freeman', born: 1937 },
+    { name: 'Cate Blanchett', born: 1969 }
+  ];
+  
+  function calculateActorsAges(actors) {
+  
+    let result = {};
+    
+  for (const item of actors) {
+    
+    if (!result[item.name]) {
+      result[item.name] = []
+    }
+    
+    result[item.name].push( "Edad : " + (2024 - item.born))
+  
+  }
+  
+  console.log(result);
+  
+  }
+  
+  
+  calculateActorsAges(actors)
